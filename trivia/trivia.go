26daadaff42ec7cb33137cd71cd7b45b3d1ecf2b
Cory Kitchens <cjkitche@calpoly.edu>
2019-01-17 15:18:46 -0800

    Submit question to backend

--- trivia/trivia.go
+++ trivia/trivia.go
@@ -14,6 +14,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/corykitchens/tp"
 	"github.com/go-chat-bot/bot"
 	"github.com/olekukonko/tablewriter"
 )
@@ -160,8 +161,9 @@ Scoring:
 }
 
 func insertQuestion(b *bot.Cmd) (string, error) {
-	q := TriviaParser(b.Message)
-	return fmt.Sprintf("Received Message -> %s", b.Message), nil
+	q := tp.TriviaParser(b.Message)
+	tp.SubmitQuestion(q)
+	return fmt.Sprintf("Post message -> %v", q), nil
 }
 
 func answer(c *bot.PassiveCmd) (string, error) {
